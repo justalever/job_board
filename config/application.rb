92b30ac2a3eaaea4623e9000dc68ab5d69645fc4
Andy Leverenz <andy@dribbble.com>
2018-03-30 19:51:12 -0500

    Initial commit

---
+++ config/application.rb
@@ -0,0 +1,20 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module JobBoard
+  class Application < Rails::Application
+    config.active_job.queue_adapter = :sidekiq
+    config.application_name = 'Job Board'
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+require 'sidekiq/web'
+
+Rails.application.routes.draw do
+  devise_for :users
+  root to: 'home#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
